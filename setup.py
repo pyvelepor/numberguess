3c400a77f88d6e7e5749642a5d63ff8bc30c9978
mew <mew>
2018-04-20 10:43:40 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='numberguess',
+    version='0.0.1',
+    modules=[''],
+    url='',
+    license='',
+    author='pyvelepor',
+    author_email='pyvelepor@gmail.com',
+    description='multiplayer number guessing game with Flask'
+)
